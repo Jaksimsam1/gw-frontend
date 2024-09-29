ba6f178001c9ced10280504f16a8f704ce5a8f67
kangseonghyeok <kangsh9820@naver.com>
2024-09-29 13:12:43 +0900

    [test] page router test

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
+
 export const Main = () => {
     return (
         <div>
-            작심삼일 develop Main Page
+            작심삼일 develop Main Page <br />
+            <Link to="/sample">샘플 테스트페이지 Go</Link>
         </div>
     )
 }
